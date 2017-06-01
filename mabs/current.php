d1336ad86398270a035872cfa44559ae799bc642
vilomilo <vilmantasbieliunas@gmail.com>
2017-06-01 14:21:47 +0300

    delete
    some shit

--- mabs/current.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-header("Access-Control-Allow-Origin: http://extrafm.lt");
-
-header("Content-Time: application/json");
-
-$f = file_get_contents('http://www.extrafm.eu/mabs/example.php');
-
-echo json_encode($f);
